4afc9b8de11f2e59046323738ddf91829af1788c
AngelGabrielG <angel.gaitan.ev@gmail.com>
2024-08-01 23:32:55 -0300

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'wanderaura.mobile',
+  appName: 'wanderaura-mobile',
+  webDir: 'www'
+};
+
+export default config;
