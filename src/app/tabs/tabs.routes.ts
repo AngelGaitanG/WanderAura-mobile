bf60e322e11babd5ea5965a242f3c292e97d2856
AngelGabrielG <angel.gaitan.ev@gmail.com>
2024-08-03 16:53:09 -0300

    setting page created

--- src/app/tabs/tabs.routes.ts
+++ src/app/tabs/tabs.routes.ts
@@ -6,6 +6,18 @@ export const TABS_ROUTES: Routes = [
         loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
     },
     {
+        path: 'profile',
+        loadComponent: () => import('./profile/profile.page').then((m) => m.ProfilePage),
+    },
+    {
+        path: 'trips',
+        loadComponent: () => import('./trips/trips.page').then((m) => m.TripsPage),
+    },
+    {
+        path: 'activities',
+        loadComponent: () => import('./activities/activities.page').then((m) => m.ActivitiesPage),
+    },
+    {
         path: '',
         redirectTo: 'home',
         pathMatch: 'full',
