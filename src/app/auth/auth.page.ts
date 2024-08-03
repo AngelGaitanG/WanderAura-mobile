3a272eed4aec900f883eadecf977c8cf55ef2be9
AngelGabrielG <angel.gaitan.ev@gmail.com>
2024-08-02 23:26:05 -0300

    login view finished

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -3,25 +3,27 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton } from '@ionic/angular/standalone';
 import { ButtonActivated } from './enum/button-activated.enum';
+import { LoginComponent } from './components/login/login.component';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.page.html',
   styleUrls: ['./auth.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, LoginComponent]
 })
 export class AuthPage {
 
   buttonActivated: string;
 
   constructor() {
-    this.buttonActivated = ButtonActivated.None;
+    this.buttonActivated = ButtonActivated.Login;
   }
 
   login() {
     this.buttonActivated = ButtonActivated.Login;
   }
+
 
   register(){
     this.buttonActivated = ButtonActivated.Register;
--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,14 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { IonItem, IonInput, IonLabel, IonButton } from '@ionic/angular/standalone';
+import { ILoginForm } from './interfaces/login.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
+  standalone: true,
+  imports: [ FormsModule, IonItem, IonInput, IonLabel, IonButton, CommonModule ],
 })
-export class LoginComponent  implements OnInit {
+export class LoginComponent  {
 
-  constructor() { }
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      const loginForm: ILoginForm = form.value;
+      console.log(loginForm); 
+    } 
+  }
 
-  ngOnInit() {}
+  constructor() {}
 
 }
