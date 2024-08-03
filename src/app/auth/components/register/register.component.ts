b5b9b016577e9675b93f768df0535cfbc9a1c9b9
AngelGabrielG <angel.gaitan.ev@gmail.com>
2024-08-02 23:38:51 -0300

    standalone ionic register form

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { IonInput, IonLabel, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
-  standalone: true
+  standalone: true,
+  imports: [IonInput, IonLabel, IonButton],
 })
 export class RegisterComponent {
 
