5c8cc408ce68bd8fb84cdc377f23571621efd8a3
AngelGabrielG <angel.gaitan.ev@gmail.com>
2024-08-03 23:16:51 -0300

    trying cards

--- src/app/tabs/home/home.page.ts
+++ src/app/tabs/home/home.page.ts
@@ -1,13 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import * as ionic from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [ionic.IonHeader, ionic.IonToolbar, ionic.IonTitle, ionic.IonContent, ionic.IonCard, ionic.IonCardContent, ionic.IonCardHeader, ionic.IonCardSubtitle, ionic.IonCardTitle, CommonModule]
 })
 export class HomePage {
+  cards = [
+    { title: 'Card 1', subtitle: 'Card subtitle', content: 'I am card 1', img: '../../assets/backgrounds/flowers.jpg' },
+    { title: 'Card 2', subtitle: 'Card subtitle', content: 'I am card 2', img: '../../assets/backgrounds/country.jpg' },
+    { title: 'Card 3', subtitle: 'Card subtitle', content: 'I am card 3', img: '../../assets/backgrounds/forest.jpg' },
+    { title: 'Card 4', subtitle: 'Card subtitle', content: 'I am card 4', img: '../../assets/backgrounds/autumn.jpg' }
+  ];
+  
   constructor() {}
 }
