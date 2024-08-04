0f8113a3af4cce59fa3fb8570f77f8ca41d3f34a
AngelGabrielG <angel.gaitan.ev@gmail.com>
2024-08-04 12:59:34 -0300

    home styles

--- src/app/tabs/home/home.page.ts
+++ src/app/tabs/home/home.page.ts
@@ -1,21 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import * as ionic from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [ionic.IonHeader, ionic.IonToolbar, ionic.IonTitle, ionic.IonContent, ionic.IonCard, ionic.IonCardContent, ionic.IonCardHeader, ionic.IonCardSubtitle, ionic.IonCardTitle, CommonModule]
+  imports: [ IonicModule, CommonModule]
 })
 export class HomePage {
   cards = [
-    { title: 'Card 1', subtitle: 'Card subtitle', content: 'I am card 1', img: '../../assets/backgrounds/flowers.jpg' },
-    { title: 'Card 2', subtitle: 'Card subtitle', content: 'I am card 2', img: '../../assets/backgrounds/country.jpg' },
-    { title: 'Card 3', subtitle: 'Card subtitle', content: 'I am card 3', img: '../../assets/backgrounds/forest.jpg' },
-    { title: 'Card 4', subtitle: 'Card subtitle', content: 'I am card 4', img: '../../assets/backgrounds/autumn.jpg' }
+    { title: 'Trips', img: '../../assets/backgrounds/flowers.jpg' },
+    { title: 'Activities', img: '../../assets/backgrounds/country.jpg' },
+    { title: 'Tours', img: '../../assets/backgrounds/forest.jpg' },
+    { title: 'Tour Guides', img: '../../assets/backgrounds/autumn.jpg' }
   ];
-  
   constructor() {}
 }
