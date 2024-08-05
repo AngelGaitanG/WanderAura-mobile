d101f9c4e80c023b4ef1f754f955cdcb2e64186b
AngelGabrielG <angel.gaitan.ev@gmail.com>
2024-08-05 18:10:39 -0300

    touch slider finished

--- src/app/commons/components/slides/touch-slider/touch-slider.component.ts
+++ src/app/commons/components/slides/touch-slider/touch-slider.component.ts
@@ -1,16 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 import { Slide } from 'src/app/commons/interfaces/Slide.interface';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-touch-slider',
   templateUrl: './touch-slider.component.html',
   styleUrls: ['./touch-slider.component.scss'],
   standalone: true,
-  imports: []
+  imports: [CommonModule ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class TouchSliderComponent {
 
   @Input() images: Slide[] = [];
+
+
   constructor() { }
 
 
