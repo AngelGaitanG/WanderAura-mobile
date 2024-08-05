b8018a037c0ebbf04918e41f006887890b70e92a
AngelGabrielG <angel.gaitan.ev@gmail.com>
2024-08-04 22:59:49 -0300

    bottom section for profile styles

--- src/app/tabs/profile/profile.page.ts
+++ src/app/tabs/profile/profile.page.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { TopSectionComponent } from './components/top-section/top-section.component';
+import { BottomSectionComponent } from './components/bottom-section/bottom-section.component';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, TopSectionComponent]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, TopSectionComponent, BottomSectionComponent]
 })
 export class ProfilePage  {
 
