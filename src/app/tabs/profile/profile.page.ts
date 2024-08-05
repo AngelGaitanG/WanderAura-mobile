e76ac308dd5aaf991286afe63b3a0815e6c380ad
AngelGabrielG <angel.gaitan.ev@gmail.com>
2024-08-04 21:16:11 -0300

    creating components for profile page

--- src/app/tabs/profile/profile.page.ts
+++ src/app/tabs/profile/profile.page.ts
@@ -2,19 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { TopSectionComponent } from './components/top-section/top-section.component';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, TopSectionComponent]
 })
-export class ProfilePage implements OnInit {
+export class ProfilePage  {
 
   constructor() { }
 
-  ngOnInit() {
-  }
 
 }
