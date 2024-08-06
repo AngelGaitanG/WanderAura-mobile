0d33939b5a94a25fca667288e2a492e9ee5ba499
AngelGabrielG <angel.gaitan.ev@gmail.com>
2024-08-06 16:55:58 -0300

    weather service created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,7 +12,7 @@ export const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'tabs',
+    redirectTo: 'auth',
     pathMatch: 'full',
   }
 ];
--- src/app/tabs/tabs.routes.ts
+++ src/app/tabs/tabs.routes.ts
@@ -31,7 +31,7 @@ export const TABS_ROUTES: Routes = [
     },
     {
         path: '',
-        redirectTo: 'activities',
+        redirectTo: 'home',
         pathMatch: 'full',
     }
 ]
