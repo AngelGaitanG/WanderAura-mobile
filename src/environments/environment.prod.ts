887644631410af9855f0214722dcd72fe7f1a916
AngelGabrielG <angel.gaitan.ev@gmail.com>
2024-08-11 20:08:34 -0300

    icons changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherApiKey: '41ae083203c7c4b9589aaf04c09f6e7d',
+  weatherApiUrl: 'https://api.openweathermap.org/data/3.0',
+  wanderAuraApi: 'https://wanda-backend.onrender.com/'
+
 };
