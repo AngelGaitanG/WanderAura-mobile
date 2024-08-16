158c89c9a0eb7e24097f9838fa8bcc64d08988b1
AngelGabrielG <angel.gaitan.ev@gmail.com>
2024-08-16 09:19:31 -0300

    payment button

---
+++ src/app/core/services/payment.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaymentService {
+
+  constructor(private http: HttpClient) { }
+
+
+  simplePayment(tripId: string):Observable<object> {
+    const body = { tripId: tripId }
+    return this.http.post<object>(`${environment.wanderAuraApi}payments/simple`, body);
+  }
+
+}
--- src/app/tabs/trips/pages/trip-detail/trip-detail.page.ts
+++ src/app/tabs/trips/pages/trip-detail/trip-detail.page.ts
@@ -1,22 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonImg, IonToolbar, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonCard, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonImg, IonToolbar, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonCard, IonButton, IonIcon } from '@ionic/angular/standalone';
 import { ActivatedRoute } from '@angular/router';
 import { TripsService } from 'src/app/core/services/trips.service';
+import { TripResponse } from 'src/app/commons/interfaces/Trips.interface';
+import { PaymentService } from 'src/app/core/services/payment.service';
 
 @Component({
   selector: 'app-trip-detail',
   templateUrl: './trip-detail.page.html',
   styleUrls: ['./trip-detail.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonCard, IonImg, IonButton, CommonModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonCard, IonImg, IonButton, CommonModule, IonIcon]
 })
 export class TripDetailPage implements OnInit {
   tripId: string | null = null;
-  trip: any = {};
+  trip: any ;
+  successPayment: boolean = false;
 
-  constructor(private route: ActivatedRoute, private tripsService: TripsService) {}
+  constructor(private route: ActivatedRoute, private tripsService: TripsService,
+    private paymentsService: PaymentService
+  ) {
+  }
 
   ngOnInit() {
     this.tripId = this.route.snapshot.paramMap.get('trip');
@@ -35,4 +41,20 @@ export class TripDetailPage implements OnInit {
       });
     }
   }
+
+  createPayment(tripId: string) {
+    this.paymentsService.simplePayment(tripId).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.successPayment = true;
+      },
+      error(err) {
+        console.error(err);
+      },
+      complete() {
+        console.log('complete');
+      },
+    })
+  }
+
 }
