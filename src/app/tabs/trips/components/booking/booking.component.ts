d9935686122fd7a68e2d39a394688b273524ac0b
AngelGabrielG <angel.gaitan.ev@gmail.com>
2024-08-27 22:00:28 -0300

    too much perfect

--- src/app/tabs/trips/components/booking/booking.component.ts
+++ src/app/tabs/trips/components/booking/booking.component.ts
@@ -1,33 +1,43 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
 import { TimeFormatPipe } from "src/app/commons/pipes/time.pipe";
-import { IonButton, IonItem, IonInput, IonLabel } from "@ionic/angular/standalone";
+import { IonButton, IonItem, IonInput, IonLabel, IonRadioGroup, IonRadio } from "@ionic/angular/standalone";
 import { CommonModule } from "@angular/common";
-import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { FormControl, FormGroup, FormsModule, NgModel, ReactiveFormsModule, Validators } from "@angular/forms";
 
 @Component({
     selector: 'app-booking',
     templateUrl: './booking.component.html',
     styleUrls: ['./booking.component.scss'],
     standalone: true,
-    imports: [TimeFormatPipe, IonButton, CommonModule, IonItem, IonInput, IonLabel, ReactiveFormsModule],
+    imports: [TimeFormatPipe, IonButton, CommonModule, IonItem, IonInput, IonLabel, ReactiveFormsModule, IonRadioGroup, IonRadio, FormsModule],
     
 })
 export class BookingComponent {
     @Input() trip: any;
     people: number = 1;
     isSecondFormActived: boolean = false;
+    isThirdFormActived: boolean = false;
     totalPrice: number = 0;
+    travelers: any[] = [];
+    actualTraveler: number = 1;
+    bodyPaymentRequest: any;
 
-    form = new FormGroup({
-        name0: new FormControl(''),
-        lastName0: new FormControl(''),
-        email0: new FormControl(''),
+    paymentForm = new FormGroup({
+        paymentMethod: new FormControl('', [Validators.required]),
+      });      
+
+    travelersForm = new FormGroup({
+        name: new FormControl('', [Validators.required]),
+        lastname: new FormControl('', [Validators.required]),
+        email: new FormControl('', [Validators.required, Validators.email]),
+        birthdate: new FormControl('', [Validators.required]),
+        passportNumber: new FormControl('', [Validators.required, Validators.pattern(/^[0-9]{6}$/)]),
+        passportExpiration: new FormControl('', [Validators.required]),
     });
-
-    constructor(){}
 
     incrementPeople() {
         this.people = this.people + 1;
+        
     }
 
     decrementPeople() {
@@ -42,9 +52,31 @@ export class BookingComponent {
         this.isSecondFormActived = true;
     }
 
+    submitTravelersForm() {
+        if(this.travelersForm.valid) {
+        this.travelers.push(this.travelersForm.value);
+        this.travelersForm.reset();
+        if(this.actualTraveler <= this.people) {
+            this.actualTraveler++;
+        }
+       
+    }   
+    }
+
+    submitToThirdForm(){
+        this.isThirdFormActived = true;
+        this.isSecondFormActived = false;
+    }
     
-    onSubmit(){
-        console.log('Formulario Enviado');
-        console.log(`Total a pagar: ${this.totalPrice}`);
+
+    submitPayment(){
+        this.bodyPaymentRequest = {
+            tripId: this.trip.id,
+            travelers: this.travelers,
+            totalPrice: this.totalPrice,
+            currency: "USD",
+        }
+
+        console.log(this.bodyPaymentRequest);
     }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   production: true,
   weatherApiKey: '41ae083203c7c4b9589aaf04c09f6e7d',
   weatherApiUrl: 'https://api.openweathermap.org/data/3.0',
-  wanderAuraApi: deployUrl
+  wanderAuraApi: localUrl
 };
 
 /*
