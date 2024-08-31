6f7149a17ef1c6214e39b5145c5973328fc79646
AngelGabrielG <angel.gaitan.ev@gmail.com>
2024-08-30 23:49:23 -0300

    bug fixed in trips routes

--- src/app/tabs/trips/pages/my-trips/my-trips.page.ts
+++ src/app/tabs/trips/pages/my-trips/my-trips.page.ts
@@ -4,14 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonImg, IonTabs, IonTabBar, IonLabel, IonTabButton, IonRouterOutlet, IonRouterLink} from '@ionic/angular/standalone';
 import { TripsService } from 'src/app/core/services/trips.service';
 import { MyTripResponse } from 'src/app/commons/interfaces/Trips.interface';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-my-trips',
   templateUrl: './my-trips.page.html',
   styleUrls: ['./my-trips.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonImg, CommonModule, IonTabs, IonTabBar, IonLabel, IonTabButton, IonRouterLink, IonRouterOutlet, RouterLink]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonImg, CommonModule, IonTabs, IonTabBar, IonLabel, IonTabButton, IonRouterLink, IonRouterOutlet, RouterLink, RouterLinkActive]
 })
 export class MyTripsPage implements OnInit {
 
--- src/app/tabs/trips/trips.routes.ts
+++ src/app/tabs/trips/trips.routes.ts
@@ -6,15 +6,20 @@ export const TRIPS_ROUTES: Routes = [
         loadComponent: () => import('./pages/my-trips/my-trips.page').then((m) => m.MyTripsPage),
     },
    {
-    path: 'my-trips/all-trips', 
+    path: 'all-trips', 
     loadComponent: () => import('./pages/my-trips/components/all-trips/all-trips.component').then((m) => m.AllTripsComponent),
    },
    {
-    path: 'my-trips/past-trips', 
+    path: 'past-trips', 
     loadComponent: () => import('./pages/my-trips/components/past-trips/past-trips.component').then((m) => m.PastTripsComponent),
    },
    {
-    path: 'my-trips/upcoming-trips',
+    path: 'upcoming-trips',
     loadComponent: () => import('./pages/my-trips/components/upcoming-trips/upcoming-trips.component').then((m) => m.UpcomingTripsComponent),
+   },
+   {
+    path: '',
+    redirectTo: 'all-trips',
+    pathMatch: 'full'
    }
 ]
